3bd9a22ca9d8191371077c4a42e7e0ac69fe8025
Fernanda Rodriguez Hernandez <fernandardzh@gmail.com>
2016-03-03 21:12:49 -0600

    segunda prueba

---
+++ choice.py
@@ -0,0 +1,21 @@
+import random
+print("vamos a jugar\n Piedra papel tijeras lagarto spock \nSalirz para salirs\n plz no escribas nada raro!")
+usuario=input("que escoges?")
+juego() = []
+usuario=usuario.lower()
+opc = ["piedra", "papel", "tijeras", "lagarto", "spock"]
+pc=opc[random.randin(0,4)]
+jugadas = {0: [["aplasta", "aplasta"],[lagarto, tijeras]], 1: [["tapa", "desautoriza"],[piedra, spock]], 2: [["corta", "decapita"],[papel, lagarto]], 3: [["envenena", "come"],[spock, papel]], 4: [["rompe", "vaporiza"],[tijeras, piedra]]}
+while usuario != "S":
+	while not juego in valores:
+    	print("que mal plan te dije que no -.- ")
+        usuario = input("que escoges?")
+	if pc in jugadas[usuario][1]:
+                print ("GANAUSUARIO")
+        elif usuario in jugadas[pc][1]:
+                print ("GANAMAQUINA")
+        else:
+                print ("EMPATE")
+
+
+    
